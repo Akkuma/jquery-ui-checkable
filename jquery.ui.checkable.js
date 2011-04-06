b433b161dc448008170ec9e4286c6f771f1984fb
Gregory Waxman <akkuma+github@gmail.com>
2011-04-06 18:31:08 -0400

    Updated to an official license. This is a modified New/Modified BSD License.

--- jquery.ui.checkable.js
+++ jquery.ui.checkable.js
@@ -1,5 +1,15 @@
-/**
-* Will put in a permanent open license, but until then no permission is given to use this code
+/*
+* jQuery UI Checkable v1.0.0
+* Copyright (c) 2011, Gregory Waxman. All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+*	1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+*	2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+*	3. The names of its contributors may not be used to endorse or promote products derived from this software without specific prior written permission.
+*	4. All private modifications should be offered to the author at https://github.com/Akkuma/jquery-ui-checkable
+*	5. All public forks must notify the author of their existence. Using github to directly fork the project will be considered sufficient notification and will fulfill this obligation.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 (function ($) {
     var NAMESPACE = 'checkable';
