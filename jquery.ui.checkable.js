be494bbc7323f6c4e51981e549bb0de8800613fe
Gregory Waxman <akkuma+github@gmail.com>
2011-10-28 17:33:11 -0400

    Updated version number to match tag

--- jquery.ui.checkable.js
+++ jquery.ui.checkable.js
@@ -1,5 +1,5 @@
 /*
-* jQuery UI Checkable v1.2.0
+* jQuery UI Checkable v1.3.0
 * Copyright (c) 2011, Gregory Waxman. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
